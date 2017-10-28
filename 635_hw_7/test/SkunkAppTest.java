87f012e389b559793713f872383bb5860990379f
evlevel <evlevel@stthomas.edu>
2017-10-28 12:03:47 -0500

    Added loaded Dice functionality

--- 635_hw_7/test/SkunkAppTest.java
+++ 635_hw_7/test/SkunkAppTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SkunkAppTest
@@ -11,11 +12,43 @@ public class SkunkAppTest
 	{
 	}
 
-	
-	@Test
+	@Ignore
 	public void test()
 	{
 		fail("my first test failure!");
+	}
+
+	@Test
+	public void new_dice_can_be_created()
+	{
+		Dice d = new Dice();
+		assertTrue("new Dice instance not null", d != null);
+	}
+
+	@Test
+	public void new_dice_will_roll_double_skunk_first()
+	{
+		Dice d = new Dice(new int[]
+		{ 1 }, new int[]
+		{ 1 });
+		d.roll();
+		assertEquals("first die is not 1", 1, d.getDie1Roll());
+		assertEquals("second die is not 1", 1, d.getDie2Roll());
+	}
+
+	@Test
+	public void new_dice_rolls_boxcars_then_double_skunk()
+	{
+		Dice d = new Dice(new int[]
+		{ 6, 1 }, new int[]
+		{ 6, 1 });
+		d.roll();
+		assertEquals("first die is not 6", 6, d.getDie1Roll());
+		assertEquals("second die is not 6", 6, d.getDie2Roll());
+		d.roll();
+		assertEquals("first die is not 1", 1, d.getDie1Roll());
+		assertEquals("second die is not 1", 1, d.getDie2Roll());
+
 	}
 
 }
